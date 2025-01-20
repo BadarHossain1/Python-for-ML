318134f7c8c85d70d1be28b65dafd620f8b514f8
Badar Hossain <badar12041@gmail.com>
2025-01-20 19:47:18 +0600

    Loop

---
+++ Conditionals.py
@@ -0,0 +1,20 @@
+age = 21
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+
+a = 15
+b = 14
+c = 12
+
+if a>b:
+  print(a)
+elif b>c:
+  print(b)
+else:
+  print(c)
+
+
---
+++ Loop.py
@@ -0,0 +1,11 @@
+# For Loop
+for i in range(1,11):
+  print(i)
+
+
+# While Loop
+i = 9
+while i<10:
+  print(i)
+  i+=1
+
